dd3e519dabe7fee22227153a4ccf39cb4fcb4f0f
fkn1017 <kosuke.fukano@gmail.com>
2022-07-23 22:40:05 +0900

    リファクタリング

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,49 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: const Color.fromARGB(255, 103, 33, 243),
       ),
-      home: const PageWebView(),
+      home: const HomeView(),
+    );
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Demo'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: const Text('PageView'),
+            trailing: const Icon(Icons.arrow_forward_outlined),
+            onTap: () => _moveTo(context, const PageWebView()),
+          ),
+          ListTile(
+            title: const Text('TabView'),
+            trailing: const Icon(Icons.arrow_forward_outlined),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('IndexedStack'),
+            trailing: const Icon(Icons.arrow_forward_outlined),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _moveTo(BuildContext context, Widget widget) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => widget,
+        fullscreenDialog: true,
+      ),
     );
   }
 }
